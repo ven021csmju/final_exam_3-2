53b77292713919c5ac8521eac5371a856b34bfe0
surapong <Peeraphat27277@gmail.com>
2026-03-06 13:40:44 +0700

    fix prisma v7 config

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from '@prisma/config';
-import 'dotenv/config';
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-    schema: 'prisma/schema.prisma',
+  schema: "prisma/schema.prisma",
+  migrate: {
     datasource: {
-        provider: 'postgresql',
-        url: process.env.DATABASE_URL
+      url: process.env.DATABASE_URL!,
     },
-    migrations: {
-        seed: 'node prisma/seed.js'
-    }
+  },
 });
