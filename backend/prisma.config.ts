ff38b596f271b5971710b085feccf7bdd52e5fa8
surapong <Peeraphat27277@gmail.com>
2026-03-06 13:49:58 +0700

    fix prisma config

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrate: {
-    datasource: {
-      url: process.env.DATABASE_URL!,
-    },
+  db: {
+    url: process.env.DATABASE_URL!,
   },
 });
